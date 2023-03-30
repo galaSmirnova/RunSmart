4c3f5835c0ca46c7f0c93a9db44ace9f8cb5bfe9
Galina <gal4enok13@gmail.com>
2023-03-30 19:47:28 +0300

    tab script

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// slick slider
+
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-next.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }      
+          ]
+
+    });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+      });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
+//tiny slider
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false,
+//     // navPosition: 'bottom'
+// });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+// });
+
+
